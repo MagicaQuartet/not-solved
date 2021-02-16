9ada2f3f657327bfe014502ffd9da933587e57d2
MagicaQuartet <dev.dskim@gmail.com>
2021-02-16 22:35:45 +0900

    Implement extension to run on category page
      카테고리별 (대회 등) 문제 목록 페이지에서 기능이 동작하도록
      구현하였습니다.

--- not_solved.js
+++ not_solved.js
@@ -5,9 +5,10 @@
   const regexBoardList = /^\/board\/search(\/[A-Za-z0-9]+)*/
   const regexBoardPage = /^\/board\/view(\/[A-Za-z0-9]+)*/
   const regexWorkbook = /^\/workbook(\/[A-Za-z0-9]+)*/
+  const regexCategory = /^\/category(\/[A-Za-z0-9]+)*/
 
   const pathname = location.pathname;
-  if (regexProblemset.test(pathname) || regexWorkbook.test(pathname)) {
+  if (regexProblemset.test(pathname) || regexWorkbook.test(pathname) || regexCategory.test(pathname)) {
     const tierImgList = document.querySelectorAll('img.solvedac-tier');
     tierImgList.forEach((elem) => {
       const accepted = elem.parentElement.parentElement.querySelector('span.label-success') !== null;
