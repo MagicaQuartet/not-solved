5f6ef340da854c10fccc8451da28156ba78f2baa
MagicaQuartet <dev.dskim@gmail.com>
2021-02-16 22:29:25 +0900

    Simplify query selector
      문제 목록, 문제집 등에서 동일한 테이블 row 내의 tier icon과 success
      label (또는 result-ac)를 매칭해야 하는 부분을 제외한 나머지의
      querySelector는 document로부터 수행하도록 하였습니다.
      퍼포먼스가 크게 중요한 단계는 아니기 때문에 가독성을 더 높이고자
      함입니다.

--- not_solved.js
+++ not_solved.js
@@ -7,43 +7,27 @@
   const regexWorkbook = /^\/workbook(\/[A-Za-z0-9]+)*/
 
   const pathname = location.pathname;
-  if (regexProblemset.test(pathname)) {
-    const problemset = document.querySelector('#problemset');
-    const tierImgList = problemset.querySelectorAll('tbody td img.solvedac-tier');
+  if (regexProblemset.test(pathname) || regexWorkbook.test(pathname)) {
+    const tierImgList = document.querySelectorAll('img.solvedac-tier');
     tierImgList.forEach((elem) => {
-      const successLabel = elem.parentElement.parentElement.querySelector('span.label-success');
-      elem.style.display = successLabel ? '' : 'none';
-    });
-  } else if (regexProblem.test(pathname)) {
-    const problemMenu = document.querySelector('.problem-menu');
-    const tierImg = problemMenu.querySelector('img.solvedac-tier');
-    const successLabel = problemMenu.parentElement.parentElement.querySelector('span.label-success');
-    tierImg.style.display = successLabel ? '' : 'none';
-  } else if (regexStatus.test(pathname)) {
-    const statusTable = document.querySelector('#status-table');
-    const tierImgList = statusTable.querySelectorAll('img.solvedac-tier');
-    tierImgList.forEach((elem) => {
-      const accepted = elem.parentElement.querySelector('a.result-ac');
+      const accepted = elem.parentElement.parentElement.querySelector('span.label-success') !== null;
       elem.style.display = accepted ? '' : 'none';
     });
-  } else if (regexBoardList.test(pathname)) {
-    const problemTitleList = document.querySelectorAll('a.problem_title');
-    console.log(problemTitleList)
-    problemTitleList.forEach((elem) => {
-      const tierIcon = elem.parentElement.querySelector('img');
-      const accepted = elem.classList.contains('result-ac');
-      tierIcon.style.display = accepted ? '' : 'none';
+  } else if (regexProblem.test(pathname)) {
+    const tierImg = document.querySelector('img.solvedac-tier');
+    const accepted = document.querySelector('span.label-success') !== null;
+    tierImg.style.display = accepted ? '' : 'none';
+  } else if (regexStatus.test(pathname) || regexBoardList.test(pathname)) {
+    const tierImgList = document.querySelectorAll('img.solvedac-tier');
+    tierImgList.forEach((elem) => {
+      const accepted = elem.parentElement.querySelector('a.result-ac') !== null;
+      elem.style.display = accepted ? '' : 'none';
     });
   } else if (regexBoardPage.test(pathname)) {
+    // hide tier icon always
     const tierIcon = document.querySelector('img.solvedac-tier');
     if (tierIcon !== null) {
       tierIcon.style.display = 'none';
     }
-  } else if (regexWorkbook.test(pathname)) {
-    const tierImgList = document.querySelectorAll('img.solvedac-tier');
-    tierImgList.forEach((elem) => {
-      const successLabel = elem.parentElement.parentElement.querySelector('span.label-success');
-      elem.style.display = successLabel ? '' : 'none';
-    });
   }
 })();
