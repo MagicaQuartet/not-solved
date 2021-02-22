10bb08a3442529eedc37c3c17275458b01dee44e
MagicaQuartet <dev.dskim@gmail.com>
2021-02-22 22:17:43 +0900

    Add regex test for added problem list, step and submit page
      추가된 문제 페이지와 단계별 문제 페이지에 기능을 적용하였고, accepted
      여부를 알 수 없는 제출 페이지에서는 항상 가리도록 regex test를
      추가하였습니다.

--- common_util.js
+++ common_util.js
@@ -5,6 +5,9 @@ const regexBoardList = /^\/board\/search(\/[A-Za-z0-9]+)*/
 const regexBoardPage = /^\/board\/view(\/[A-Za-z0-9]+)*/
 const regexWorkbook = /^\/workbook(\/[A-Za-z0-9]+)*/
 const regexCategory = /^\/category(\/[A-Za-z0-9]+)*/
+const regexProblemAdded = /^\/problem\/added(\/[A-Za-z0-9]+)*/
+const regexStep = /^\/step(\/[A-Za-z0-9]+)*/;
+const regexSubmit = /^\/submit(\/[A-Za-z0-9]+)*/;
 
 const pathname = location.pathname;
 
@@ -22,7 +25,7 @@ function toggleTierIconForUnsolved() {
 }
 
 function hideTierIconForUnsolved() {
-  if (regexProblemset.test(pathname) || regexWorkbook.test(pathname) || regexCategory.test(pathname)) {
+  if (regexProblemset.test(pathname) || regexWorkbook.test(pathname) || regexCategory.test(pathname) || regexProblemAdded.test(pathname) || regexStep.test(pathname)) {
     const tierImgList = document.querySelectorAll('img.solvedac-tier');
     tierImgList.forEach((elem) => {
       const accepted = elem.parentElement.parentElement.querySelector('span.label-success') !== null;
@@ -44,7 +47,7 @@ function hideTierIconForUnsolved() {
         elem.classList.add('solvedac-tier-hidden');
       }
     });
-  } else if (regexBoardPage.test(pathname)) {
+  } else if (regexBoardPage.test(pathname) || regexSubmit.test(pathname)) {
     // hide tier icon always
     const tierIcon = document.querySelector('img.solvedac-tier');
     if (tierIcon !== null) {
